253ce58f3be6802b1a9fcc490ab483cbe6e86f33
Vinicius Tonial Sossella <viniciussossella@gmail.com>
2017-10-25 16:24:53 -0200

    added error support

--- materialcomponents/src/main/java/com/vsossella/materialcomponents/FloatingEditText.java
+++ materialcomponents/src/main/java/com/vsossella/materialcomponents/FloatingEditText.java
@@ -4,9 +4,8 @@ import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Canvas;
-import android.graphics.drawable.Drawable;
 import android.text.Editable;
+import android.text.InputFilter;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.util.TypedValue;
@@ -17,6 +16,10 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+
 
 /**
  * Created by vsossella on 23/10/17.
@@ -26,21 +29,57 @@ public class FloatingEditText extends RelativeLayout {
 
     LayoutInflater mInflater;
     View layoutView;
+    boolean animateUp = true;
+    boolean isFirstTime = true;
+    boolean shouldAnimateOnEnterText = true;
+    boolean shouldAnimateOnCleanText = true;
+    boolean applyDecimalMask = false;
+
+
+    public void addMensagemErro(String mensagemErro) {
+
+        getTextViewError().setText(mensagemErro);
+
+        if (mensagemErro == null || mensagemErro.isEmpty()) {
+            getTextViewError().setVisibility(INVISIBLE);
+            getUnderlineEditText().setBackgroundColor(getResources().getColor(R.color.white));
+        } else {
+            getTextViewError().setVisibility(VISIBLE);
+            getUnderlineEditText().setBackgroundColor(getResources().getColor(R.color.red));
+        }
+
+    }
+
+    public void clearMensagemErro() {
+        getTextViewError().setText("");
+        getTextViewError().setVisibility(INVISIBLE);
+        getUnderlineEditText().setBackgroundColor(getResources().getColor(R.color.white));
+    }
+
+    TextView floatingLabel;
+    EditText editText;
+    View underlineEditText;
 
     public FloatingEditText(Context context) {
         super(context);
     }
 
     public FloatingEditText(Context context, AttributeSet attrs) {
+
         super(context, attrs);
         mInflater = LayoutInflater.from(context);
 
         init();
         loadCustomAttributeSet(context, attrs);
+        addAnimation();
     }
 
     private TextView getFloatingTextView() {
         return (TextView) layoutView.findViewById(R.id.text_view_hint_material);
+    }
+
+    private TextView getTextViewError() {
+        return (TextView) layoutView.findViewById(R.id.text_view_error);
     }
 
     private EditText getEditText() {
@@ -51,49 +90,131 @@ public class FloatingEditText extends RelativeLayout {
         return (ImageView) layoutView.findViewById(R.id.image_view_material_edit_text);
     }
 
+    private View getUnderlineEditText() {
+        return layoutView.findViewById(R.id.edit_text_under_line);
+    }
+
+
     private void init() {
         layoutView = mInflater.inflate(R.layout.floating_edit_text, this, true);
-        addAnimation();
+        floatingLabel = getFloatingTextView();
+        editText = getEditText();
+        underlineEditText = getUnderlineEditText();
     }
 
     private void addAnimation() {
+        getEditText().addTextChangedListener(animationFloatingTextViewWatcher);
+
+        if (applyDecimalMask)
+            getEditText().addTextChangedListener(currencyFormatWatcher);
+    }
+
+    private TextWatcher currencyFormatWatcher = new TextWatcher() {
+        private String current = "";
+
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            if (!s.toString().equals(current)) {
+                clearMensagemErro();
+                getEditText().removeTextChangedListener(this);
+
+                String cleanString = s.toString().replaceAll("[$,.]", "");
+
+                double parsed = Double.parseDouble(cleanString);
+                String formatted = parsed == 0 ? "" : getNumberFormat().format((parsed / 100));
+
+                current = formatted;
+                getEditText().setText(formatted);
+                getEditText().setSelection(formatted.length());
+
+                getEditText().addTextChangedListener(this);
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
+
+    private TextWatcher animationFloatingTextViewWatcher = new TextWatcher() {
+
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            if (isFirstTime) {
+                isFirstTime = false;
+                return;
+            }
+
+            if (s.length() > 0 && shouldAnimateOnEnterText) {
+                shouldAnimateOnEnterText = false;
+                shouldAnimateOnCleanText = true;
+                if (animateUp) {
+                    animateField(editText.getY() - 30f, 100f);
+                } else {
+                    setupRightYPosition(underlineEditText.getY() - 10f);
+                    animateField(underlineEditText.getY() + 10f, 100f);
+                }
+            }
+
+
+            if (s.length() == 0 && shouldAnimateOnCleanText) {
+                shouldAnimateOnCleanText = false;
+                shouldAnimateOnEnterText = true;
+                if (animateUp) {
+                    animateField(floatingLabel.getY() + 10f, 0f);
+                } else {
+                    animateField(underlineEditText.getY() - 10f, 0f);
+                }
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
+
+    private void setupRightYPosition(float yPosition) {
+        getFloatingTextView().setY(yPosition);
+    }
+
+    private void animateField(float yPosition, float alpha) {
         final TextView floatingLabel = getFloatingTextView();
-        final EditText editText = getEditText();
 
-        editText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        ObjectAnimator moveUp = ObjectAnimator.ofFloat(floatingLabel, "y", yPosition);
+        ObjectAnimator showAnim = ObjectAnimator.ofFloat(floatingLabel, "alpha", alpha);
+        showAnim.setDuration(100);
+        moveUp.setDuration(100);
+        AnimatorSet animationFadein = new AnimatorSet();
+        animationFadein.play(moveUp).with(showAnim);
+        animationFadein.start();
+    }
 
-            }
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(count == 1 && before == 0) {
-                    ObjectAnimator moveUp = ObjectAnimator.ofFloat(floatingLabel, "y", editText.getY() - 30f);
-                    ObjectAnimator showAnim = ObjectAnimator.ofFloat(floatingLabel, "alpha", 100f);
-                    showAnim.setDuration(100);
-                    moveUp.setDuration(100);
-                    AnimatorSet animationFadein = new AnimatorSet();
-                    animationFadein.play(moveUp).with(showAnim);
-                    animationFadein.start();
-                }
-
-                if(start == 0 && count == 0) {
-                    ObjectAnimator moveDown = ObjectAnimator.ofFloat(floatingLabel, "y", floatingLabel.getY() + 10f);
-                    ObjectAnimator hideAnim = ObjectAnimator.ofFloat(floatingLabel, "alpha", 0f);
-                    hideAnim.setDuration(100);
-                    moveDown.setDuration(100);
-                    AnimatorSet animationFadeout = new AnimatorSet();
-                    animationFadeout.play(moveDown).with(hideAnim);
-                    animationFadeout.start();
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        });
+    private NumberFormat getNumberFormat() {
+        NumberFormat formatter = NumberFormat.getCurrencyInstance();
+        DecimalFormatSymbols dfs = new DecimalFormatSymbols();
+        dfs.setCurrencySymbol("");
+        dfs.setGroupingSeparator('.');
+        dfs.setMonetaryDecimalSeparator(',');
+        ((DecimalFormat) formatter).setNegativePrefix("-");
+        ((DecimalFormat) formatter).setDecimalFormatSymbols(dfs);
+        return formatter;
     }
 
     private void loadCustomAttributeSet(Context context, AttributeSet attrs) {
@@ -106,9 +227,19 @@ public class FloatingEditText extends RelativeLayout {
             getFloatingTextView().setText(floatingLabelText);
             getEditText().setHint(floatingLabelText);
 
+            String editTextError = attributes.getString(R.styleable.FloatingEditText_edit_text_error);
+            addMensagemErro(editTextError);
 
+            animateUp = attributes.getBoolean(R.styleable.FloatingEditText_edit_text_animate_up, false);
+            applyDecimalMask = attributes.getBoolean(R.styleable.FloatingEditText_edit_text_decimal_mask, false);
             float editTextFontSize = attributes.getDimensionPixelSize(R.styleable.FloatingEditText_edit_text_font_size, 16);
             getEditText().setTextSize(TypedValue.COMPLEX_UNIT_PX, editTextFontSize);
+
+            int editTextMaxLength = attributes.getInteger(R.styleable.FloatingEditText_edit_text_max_length, 150);
+            InputFilter[] filterArray = new InputFilter[1];
+            filterArray[0] = new InputFilter.LengthFilter(editTextMaxLength);
+            getEditText().setFilters(filterArray);
+
 
             String editTextValue = attributes.getString(R.styleable.FloatingEditText_edit_text_value);
             getEditText().setText(editTextValue);
